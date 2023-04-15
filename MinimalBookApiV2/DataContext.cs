78ccad35102e90711cc548d05a2288e0e6c13415
johannpa <johann_paque@hotmail.fr>
2023-04-15 23:42:55 +0200

    MinimalBookApi version 2 with database

---
+++ MinimalBookApiV2/DataContext.cs
@@ -0,0 +1,18 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace MinimalBookApiV2
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+        
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress; Database=minimalbookbd; Trusted_Connection=True;TrustServerCertificate=true;");
+        }
+    }
+}
